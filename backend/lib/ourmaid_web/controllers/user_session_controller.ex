34001aec5eacf5fb561d151f99fe161e894b922d
Diego Hernández Ureño <44782479+DiegoUreno@users.noreply.github.com>
2023-04-19 12:08:48 -0600

    user auth done :cd:
    jwt has been substituted for auth gen phoenix

---
+++ backend/lib/ourmaid_web/controllers/user_session_controller.ex
@@ -0,0 +1,29 @@
+defmodule OurmaidWeb.UserSessionController do
+  use OurmaidWeb, :controller
+
+  alias Ourmaid.Accounts
+  alias OurmaidWeb.UserAuth
+
+  def new(conn, _params) do
+    render(conn, "new.html", error_message: nil)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+
+    IO.inspect(user_params)
+    %{"username" => username, "password" => password} = user_params
+
+    if user = Accounts.get_user_by_username_and_password(username, password) do
+      UserAuth.log_in_user(conn, user, user_params)
+    else
+      # In order to prevent user enumeration attacks, don't disclose whether the username is registered.
+      render(conn, "new.html", error_message: "Invalid username or password")
+    end
+  end
+
+  def delete(conn, _params) do
+    conn
+    |> put_flash(:info, "Logged out successfully.")
+    |> UserAuth.log_out_user()
+  end
+end
