ff817321d7313c226eba49b1d211288f0518d721
Diego Hernández Ureño <44782479+DiegoUreno@users.noreply.github.com>
2023-04-19 09:58:38 -0600

    Making it all again because Ramiro was right :angry:

--- backend/lib/ourmaid_web/auth_pipeline.ex
+++
@@ -1,9 +0,0 @@
-defmodule Ourmaid.Guardian.AuthPipeline do
-  use Guardian.Plug.Pipeline, otp_app: :ourmaid,
-  module: Ourmaid.Guardian,
-  error_handler: Ourmaid.AuthErrorHandler
-
-  plug Guardian.Plug.VerifyHeader, realm: "Bearer"
-  plug Guardian.Plug.EnsureAuthenticated
-  plug Guardian.Plug.LoadResource
-end
