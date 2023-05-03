12dbf5f8c9a25aa3a0afedb40a51f12d866c3826
Diego Hernández Ureño <44782479+DiegoUreno@users.noreply.github.com>
2023-05-03 14:52:50 -0600

    CA-Cert added

--- backend/config/prod.exs
+++ backend/config/prod.exs
@@ -11,6 +11,7 @@ import Config
 # before starting your production server.
 config :ourmaid, OurmaidWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
+
 # Do not print debug messages in production
 config :logger, level: :info
 
