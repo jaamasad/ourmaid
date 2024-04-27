90ae79223417d626fbeae7d1367c924cbb8c2007
jaamasad <jaamasad1@gmail.om>
2024-04-27 08:53:46 +0500

    adding goolge tag

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -11,6 +11,16 @@ export default function App({ Component, pageProps }) {
       <MainNavbar />
       <Component {...pageProps} />
       <Footer />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-7YM1Y5WW5M"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+            gtag('config', 'AW-11398284107');
+          `,
+        }}
+      />
     </>
   );
 }
